2da4f76d10a663ad05646f40f3f5e8b3ff0b75ad
SPQRw <long.1996@bk.ru>
2023-09-04 14:18:29 +0600

    cw8

---
+++ lesson8/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <router-view :title="items"></router-view>
+  <ul>
+    <li><router-link to="/first">To Items</router-link></li>
+    <!-- <li><router-link to="/second">hello Page 2</router-link></li>
+    <li><router-link to="/third">hello Page 3</router-link></li> -->
+  </ul>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+ 
+  }, 
+  data (){
+    return {
+      items:[
+        {
+          id: 0,
+          name: "IPhone13",
+          price: 500000
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.firstCase{
+  width: 150px;
+  height: 200px;
+  border: solid 1px;
+  margin-left: 20px;
+}
+</style>
---
+++ lesson8/src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- <router-view :title="items"></router-view> -->
+  <ul>
+    <li> <router-link to="/second">To trash</router-link></li>
+  </ul>
+  <!-- <div class="firstCase">
+    <img src="" alt="">
+    <h3>Iphone 13</h3>
+    <span>500,000 KZT</span>
+    <button @click="Create('iPhone13',500000)">Купить</button>
+  </div> -->
+  <div>
+    <ul>
+      <li v-for="item in title" :key="item">
+        {{ item.id }} <br>
+        {{ item.name }} <br>
+        {{ item.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: Array
+  },
+  data(){
+    return{
+      items:[]
+      }
+    },
+  methods:{
+     Create(Name, itemPrice){
+      this.items.push(
+        {
+          id: this.items.length ,
+          name: Name,
+          price: itemPrice,
+        }
+        ) 
+        console.log(this.items)
+      },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
